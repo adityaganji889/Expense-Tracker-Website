aace6f708ee7bb553d29d60860578b1de8f695ee
Aditya Ganji <adityaganji889@gmail.com>
2023-07-16 18:20:26 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 const path = require("path");
 require("dotenv").config()
 const db = require("./config/db");
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000
 const userRoute = require("./routes/userRoutes");
 const transactionRoute = require('./routes/transactionRoutes')
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json({
     limit: "50mb",
 }));
--- frontend/src/apicalls/index.js
+++ frontend/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:10000',
+    // baseURL: 'http://localhost:10000',
     headers: {
        'authorization': `Bearer ${localStorage.getItem('token')}`
     }
