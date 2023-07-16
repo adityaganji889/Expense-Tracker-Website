5238a43071f262c03fe373bc5e9f081fe5c89a46
Aditya Ganji <adityaganji889@gmail.com>
2023-07-16 17:14:50 +0530

    Pushed Files For Production

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express();
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config()
 const db = require("./config/db");
 const port = process.env.PORT || 8080
@@ -11,9 +12,21 @@ app.use(cors());
 app.use(express.json({
     limit: "50mb",
 }));
+app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/users",userRoute);
 app.use("/api/transactions",transactionRoute);
+
+
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, "/frontend/build")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(_dirname, "/frontend/build/index.html"))
+});
+
+app.use((err, req, res, next)=>{
+    res.status(500).send({ message: err.message});
+})
 
 app.listen(port,()=>{
     console.log(`Server is running on port : ${port}`);
